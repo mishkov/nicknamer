cba59de8aa4bf5531c2d5815c447398382ef721d
Nikita Mishkov <mishkov.dd@gmail.com>
2022-09-01 12:18:55 +0300

    set max width for snackbar

--- lib/custom_toast/custom_toast.dart
+++ lib/custom_toast/custom_toast.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 void showToast(BuildContext context, String message) {
@@ -7,7 +9,16 @@ void showToast(BuildContext context, String message) {
   final padding = EdgeInsets.all(8);
   final duration = Duration(milliseconds: 600);
 
+  final messengerWidth = ScaffoldMessenger.of(context).context.size?.width;
+  double? snackBarWidth;
+  if (messengerWidth != null) {
+    const maxSnackBarWidth = 300.0;
+    snackBarWidth = math.min(messengerWidth, maxSnackBarWidth);
+  }
+
   final snackBar = SnackBar(
+    behavior: SnackBarBehavior.floating,
+    width: snackBarWidth,
     duration: duration,
     elevation: 0,
     backgroundColor: Colors.transparent,
